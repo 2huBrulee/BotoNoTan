9f5a4633eabde837cb508944d39d06bc55330842
2huBrulee <alejandro2hpxa@gmail.com>
2019-10-16 01:30:09 -0500

    Window Capture

---
+++ Desire/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace MilleniumEye
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            var doro = new Doro();
+            doro.CaptureProcessWindow(Process.GetProcessesByName("dlpc")[0]);
+            InitializeComponent();
+        }
+    }
+}
--- MilleniumEye/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MilleniumEye
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
